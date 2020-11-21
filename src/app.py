b662950c8ae28386720db9b726eb965d98e7554d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-11-21 12:30:08 -0500

    Creating Database with MySQL and Models

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_marshmallow import Marshmallow
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='RXuwGX6syfZoZ4gi$y8fbkEtDgknmsAcpUyVQRmo'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Br1tnet$2=@localhost/ginger_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+ma=Marshmallow(app)
+
+from controller import *
+
+if __name__=='__main__':
+  app.run(debug=True)
---
+++ src/models.py
@@ -0,0 +1,29 @@
+from app import db,ma
+
+class User(db.Model):
+  id=db.Column(db.Integer, primary_key=True)
+  public_id=db.Column(db.String(50), unique=True)
+  name=db.Column(db.String(50))
+  email=db.Column(db.String(128))
+  username=db.Column(db.String(50))
+  password=db.Column(db.String(100))
+  avatar=db.Column(db.String(512))
+  admin=db.Column(db.Boolean)
+
+  def __init__(self, public_id,name,email,username,password,avatar,admin):
+    self.public_id=public_id
+    self.name=name
+    self.email=email 
+    self.username=username
+    self.password=password
+    self.avatar=avatar
+    self.admin=admin
+
+db.create_all()
+
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('id','public_id','name','email','username','password','avatar','admin')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
