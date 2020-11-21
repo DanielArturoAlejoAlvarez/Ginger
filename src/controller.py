13fb06de6ca66728b64c1a58e86200480b2ef1c7
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-11-21 18:43:46 -0500

    Configure our authentication with JWT

--- src/controller.py
+++ src/controller.py
@@ -4,10 +4,13 @@ from flask import request,jsonify,make_response
 
 from models import *
 
-import uuid 
+import uuid
+
+import jwt
+
+import datetime
 
 from werkzeug.security import generate_password_hash, check_password_hash
-
 
 # User Controller
 @app.route('/users', methods=['GET'])
@@ -72,6 +75,30 @@ def delete_user(id):
   db.session.commit()
 
   return user_schema.jsonify(user)
+
+
+# Apply Authentication
+@app.route('/login')
+def login():
+  auth=request.authorization 
+
+  if not auth or not auth.username or not auth.password:
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+  user = User.filter_by(name=auth.username).first()
+
+  if not user:
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+  if check_password_hash(user.password, auth.password):
+    token=jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+    return jsonify({'token': token.decode('UTF-8')})
+
+  return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+
+
 
 
 # Todo Controller
